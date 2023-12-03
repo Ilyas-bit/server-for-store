2f9daa00d32d270b6e702d5f30cb53d2f4594a5d
Ilyas <qazwsxedc21012003@gmail.com>
2023-12-03 22:26:48 +0500

    добавил CORS

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // Middleware для CORS
 app.use(bodyParser.json());
 
 // Подключение к MongoDB Atlas (используем переменную окружения для строки подключения)
